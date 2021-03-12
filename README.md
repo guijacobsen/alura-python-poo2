6d41c2be562a69f748d6f5dc79b2e4c0b1c58914
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-03-11 23:21:11 -0300

    - Herança - Construtor pai - Desfazendo objeto "privado"

---
+++ README.md
@@ -0,0 +1,11 @@
+# WIP
+
+## Python OO
+
+Orientação a Objeto em Python 3 acompanhando o curso da Alura. (parte 2)
+
+#
+
+### Executando o código
+
+Para executar, rodar no terminal: `python3 modelo`.
--- modelo.py
+++ modelo.py
@@ -1,52 +1,45 @@
-class Filme:
+class Programa:
+  def __init__(self, nome, ano):
+    self._nome = nome.title()
+    self.ano = ano
+    self._likes = 0
+
+  def dar_like(self):
+    self._likes += 1
+
+  @property
+  def likes(self):
+    return self._likes
+
+  @property
+  def nome(self):
+    return self._nome
+
+  @nome.setter
+  def nome(self, nome):
+    self._nome = nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-      self.__nome = nome.title()
+      self._nome = nome.title()
       self.ano = ano
       self.duracao = duracao
-      self.__likes = 0
+      self._likes = 0
 
-    def dar_like(self):
-      self.__likes += 1
-
-    @property
-    def likes(self):
-      return self.__likes
-
-    @property
-    def nome(self):
-      return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-      self.__nome = nome.title()
-    
-
-class Seria:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-      self.__nome = nome.title()
+      self._nome = nome.title()
       self.ano = ano
       self.temporadas = temporadas
-      self.__likes = 0
+      self._likes = 0
 
-    def dar_like(self):
-      self.__likes += 1
-    
-    @property
-    def likes(self):
-      return self.__likes
-    
-    @property
-    def nome(self):
-      return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-      self.__nome = nome.title()
+   
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
 
-atlanta = Seria('atlanta', 2018, 2)
+atlanta = Serie('atlanta', 2018, 2)
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
 
 
