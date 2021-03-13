9d47f093bf54649e62871ea5bc64c11af97dbfef
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-03-13 19:16:46 -0300

    - Aprendendo Mixins - Classe extendida com comportamentos padrões

--- funcionario.py
+++ funcionario.py
@@ -1,11 +1,9 @@
+# Tipos funcionarios
 class Funcionario:
-  def registra_horas(self, horas):
-    print('Horas registradas...')
-  
-  def mostrar_tarefas(self):
-    print('Fez muita coisa...')
 
-class Funcionario2:
+  def __init__(self,nome):
+    self.nome = nome.title()
+
   def registra_horas(self, horas):
     print('Horas registradas...')
   
@@ -20,26 +18,40 @@ class FuncionarioCaelum(Funcionario):
     print('Mostrando cursos - {mes}' if mes else 'Mostrando cursos desse mes')
 
 class FuncionarioAlura(Funcionario):
-  # def mostrar_tarefas(self):
-  #   print('Fez muita coisa Alura')
+  def mostrar_tarefas(self):
+    print('Fez muita coisa Alura')
   
   def busca_perguntas_sem_respostas(self):
     print('Mostrando perguntas nao respondidas no forum')
 
+class Hispter:
+  def __str__(self):
+    return f'Hispter, {self.nome}'
 
+# Niveis funcionarios
 class Junior(FuncionarioAlura):
   pass
 
 class Pleno(FuncionarioAlura,FuncionarioCaelum):
   pass
 
+class Senior(FuncionarioAlura, FuncionarioCaelum, Hispter):
+  pass
 
-# joao = Junior()
+
+# funcionarios
+joao = Junior('joao')
+guilherme = Pleno('guilherme')
+pedro = Senior('pedro')
+
+# print(f'{joao.nome}')
+# print(f'{guilherme.nome}')
+# print(f'{pedro.nome}')
+
+print(f'str guilherme:  {str(guilherme)}')
+print(f'str pedro:  {str(pedro)}')
+
+
 # joao.busca_perguntas_sem_respostas()
-
-
-guilherme = Pleno()
-# guilherme.busca_cursos_do_mes()
 # guilherme.busca_perguntas_sem_respostas()
-
-guilherme.mostrar_tarefas()
+# guilherme.mostrar_tarefas()
